0837dfe9e604a49743c917f671ab0583dfe2dc8b
Sergei Ivankov <sergeiivankov@protonmail.com>
2021-12-04 01:02:41 +0300

    Express.js webhooks receiving example

---
+++ examples/webhooks-express-receiving.js
@@ -0,0 +1,20 @@
+const { readFileSync } = require('fs');
+const { createServer } = require('https');
+const express = require('express');
+const CrypoBotAPI = require('../lib');
+
+const app = express();
+const client = new CrypoBotAPI('1234:AAA...AAA', 'testnet');
+
+client.on('paid', (invoice, requestDate) => {
+  console.log(requestDate, invoice);
+});
+
+app.use('/secret', client.middleware());
+
+// Important: at the time of publication of version 0.1.0 (Dec 4, 2021),
+// test API servers do not accept self-signed certificates
+https.createServer({
+  key: readFileSync('/etc/letsencrypt/live/studiot.ru/privkey.pem'),
+  cert: readFileSync('/etc/letsencrypt/live/studiot.ru/fullchain.pem')
+}, app).listen(443);
