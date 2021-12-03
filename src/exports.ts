1f66d6fb1fc4e6890c1c499cc99f3a78993c1763
Sergei Ivankov <sergeiivankov@protonmail.com>
2021-12-04 00:01:33 +0300

    Add export to documentation public used Node.js structures

--- src/exports.ts
+++ src/exports.ts
@@ -1,5 +1,10 @@
 // File using to generate documentation for all project entities, except only external
 
+export { ListenOptions } from 'net';
+export { ServerOptions as httpServerOptions } from 'http';
+export { Server, ServerOptions } from 'https';
+export { SecureContextOptions, TlsOptions } from 'tls';
+
 // eslint-disable-next-line import/prefer-default-export
 export { default as Client } from './classes/Client';
 export { default as ClientEmitter } from './classes/ClientEmitter';
